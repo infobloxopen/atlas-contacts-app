c1bd9f3bc51c6eb988773f477b0e826c00fd8112
Zachary Eddy <zeddy@infoblox.com>
2018-06-04 15:07:17 -0700

    Launch gRPC server, gateway, and Postgres database for e2e tests

---
+++ integration/main_integration_test.go
@@ -0,0 +1,81 @@
+// +build integration
+
+package integration
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/exec"
+	"testing"
+	"time"
+
+	migrate "github.com/infobloxopen/atlas-contacts-app/db"
+)
+
+const (
+	pkgGateway = "../cmd/gateway"
+	pkgServer  = "../cmd/server"
+)
+
+var (
+	dbTest = PostgresDBConfig{
+		Host:            "localhost",
+		Port:            5425,
+		DBUser:          "postgres",
+		DBPassword:      "postgres",
+		DBName:          "contacts",
+		migrateFunction: migrate.MigrateDB,
+	}
+)
+
+// TestMain launches a gRPC server, REST gateway, and Postgres database
+func TestMain(m *testing.M) {
+	// start the test postgres container; stop the container when finished
+	defer dbTest.RunAsContainer("contacts-application-test")()
+
+	// build the gateway and server binaries; delete binaries when finished
+	defer BuildSource(pkgServer, "server")()
+	defer BuildSource(pkgGateway, "gateway")()
+
+	// run the gateway and server binaries; stop processes when finished
+	defer RunBinary("server", "-db", dbTest.GetDSN())()
+	defer RunBinary("gateway")()
+
+	// sleep to avoid sending requests to servers that are still starting
+	log.Print("waiting for servers to start")
+	time.Sleep(time.Second * 1)
+
+	m.Run()
+}
+
+// RunBinary runs a target binary with a set of arguments provided by the user
+func RunBinary(binPath string, args ...string) func() {
+	log.Printf("running the %s binary", binPath)
+	ctx, cancel := context.WithCancel(context.Background())
+	if err := exec.CommandContext(ctx, binPath, args...).Start(); err != nil {
+		log.Fatalf("unable to start the %s binary: %v", binPath, err)
+	}
+	return func() {
+		log.Printf("stopping %s process", binPath)
+		cancel()
+	}
+}
+
+// BuildSource builds a target Go package and gives the resulting binary
+// some user-defined name
+func BuildSource(packagePath, output string) func() {
+	log.Printf("creating %s binary", output)
+	cmdBuild := exec.Command("go", "build", "-o", output, packagePath)
+	if out, err := cmdBuild.CombinedOutput(); err != nil {
+		log.Fatalf("unable to build the %s package: %v (%s)",
+			packagePath, err, string(out),
+		)
+	}
+	return func() {
+		if err := os.Remove(output); err != nil {
+			log.Fatalf("unable to delete the %s binary: %v", output, err)
+		}
+		log.Printf("deleting %s binary", output)
+	}
+}
