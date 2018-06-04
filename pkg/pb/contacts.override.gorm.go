91ee82dca3bef1a8db5e412f3cc30a9f6560aa53
Roman Azami <romanazami@outlook.com>
2018-06-04 11:50:55 -0400

    Custom examples and updated the server to show logging of when the db is starting a connection and when it connects successfully. Provided an example of a hook usage, direct db access, and how to override a crud method.

--- pkg/pb/contacts.override.gorm.go
+++ pkg/pb/contacts.override.gorm.go
@@ -1,6 +1,14 @@
 package pb
 
-import "golang.org/x/net/context"
+import (
+	"fmt"
+
+	"github.com/infobloxopen/atlas-app-toolkit/rpc/errdetails"
+	"github.com/jinzhu/gorm"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
 
 // BeforeToORM will add the primary e-mail to the list of e-mails if it isn't
 // present already
@@ -29,4 +37,51 @@ func (m *ContactORM) AfterToPB(ctx context.Context, c *Contact) error {
 		}
 	}
 	return nil
+}
+
+// Hooks and direct db access:
+// Hooks Documentation: http://gorm.io/docs/hooks.html
+// Database access: http://gorm.io/docs/sql_builder.html
+// Below is an example that shows how to add a hook that runs before the Update method and how to access the database directly.
+
+// BeforeUpdate runs before the Update method and checks to see if the ID provided already exists.
+func (m *ContactORM) BeforeUpdate(scope *gorm.Scope) (err error) {
+	// Create a new db access point
+	dbc := scope.NewDB()
+
+	// To execute commands directly :
+	// dbc.Exec("SOME SQL COMMAND")
+
+	// Executes the raw sql cmd and gets query response in rows
+	rows, err := dbc.Raw("SELECT * FROM contacts WHERE id=" + fmt.Sprint(m.Id)).Rows() // (*sql.Rows, error)
+	defer rows.Close()
+	if err != nil {
+		return err
+	}
+	// Check to see if a row with id already exists
+	if rows.Next() {
+		// Contact exists proceed to default update method
+		return nil
+	} else {
+		// Contact doesn't exist send error
+		st := status.Newf(codes.Internal, "The contact with that ID doesn't exist.")
+		return st.Err()
+	}
+}
+
+// Overriding CRUD Methods:
+// For the example below we will be overriding the Read method.
+// To override a CRUD method we need to find the Custom method (CustomCreate, CustomRead, CustomUpdate, CustomDelete)
+// from the gorm file (contacts.pb.gorm.go) and implement it below.
+
+// CustomRead method overrides the default Read function and adds custom errors with multiple details.
+func (m *ContactsDefaultServer) CustomRead(ctx context.Context, req *ReadContactRequest) (*ReadContactResponse, error) {
+	res, err := DefaultReadContact(ctx, &Contact{Id: req.GetId()}, m.DB)
+	if err != nil {
+		st := status.Newf(codes.Internal, "Unable to read contact. Error %v", err)
+		st, _ = st.WithDetails(errdetails.New(codes.Internal, "CustomRead", "Custom error message"))
+		st, _ = st.WithDetails(errdetails.New(codes.Internal, "CustomRead", "Another custom error message"))
+		return nil, st.Err()
+	}
+	return &ReadContactResponse{Result: res}, nil
 }
