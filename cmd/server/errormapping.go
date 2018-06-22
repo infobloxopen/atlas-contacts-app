cd98ba8fa8c2398c0409fa75600319b9808af417
aliaksandr_skurydzin <askurydzin@localhost.localdomain>
2018-06-22 13:17:02 +0300

    update gopkg.lock and append several errormappings

--- cmd/server/errormapping.go
+++ cmd/server/errormapping.go
@@ -6,6 +6,7 @@ import (
 	"google.golang.org/grpc/codes"
 
 	"context"
+	"regexp"
 )
 
 var ErrorMappings = []errors.MapFunc{
@@ -16,6 +17,18 @@ var ErrorMappings = []errors.MapFunc{
 		).WithField(
 			"path/id", "the specified object was not found."),
 	),
+
+	errors.NewMapping(
+		errors.CondHasPrefix("pq:"),
+		errors.MapFunc(func (ctx context.Context, err error) (error, bool) {
+			if res := regexp.MustCompile(`column "(\w+)" does not exist`).FindStringSubmatch(err.Error()); len(res) > 0 {
+				return errors.NewContainer(codes.InvalidArgument, "Invalid collection operator parameter %q.", res[1]), true
+			}
+
+			return nil, false
+		}),
+	),
+
 	errors.NewMapping(
 		// Here CondAnd without condition functions serves as 'default'.
 		errors.CondAnd(),
