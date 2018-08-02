c287bb0407ebd48630b2e98dac92e702ac97d76c
Evgeniy-L <evgeniyl@infoblox.com>
2018-08-02 20:04:44 +0300

    Apply changes screwed up during rebase + address review comments

--- db/migrations/0003_groupprofileaddress.up.sql
+++ db/migrations/0003_groupprofileaddress.up.sql
@@ -1,4 +1,4 @@
-create table profiles
+CREATE TABLE profiles
 (
   id serial primary key,
   account_id text,
@@ -6,34 +6,33 @@ create table profiles
   notes text
 );
 
-create table groups
+CREATE TABLE groups
 (
-	id serial primary key,
-	account_id text,
-	name text,
-	notes text,
-	profile_id int REFERENCES profiles(id)
+  id serial primary key,
+  account_id text,
+  name text,
+  notes text,
+  profile_id int REFERENCES profiles(id) ON DELETE SET NULL
 );
 
-
-create table group_contacts
+CREATE TABLE group_contacts
 (
-	group_id int REFERENCES groups(id) ON DELETE CASCADE,
-	contact_id int REFERENCES contacts(id) ON DELETE CASCADE,
-	primary key (group_id, contact_id)
+  group_id int REFERENCES groups(id) ON DELETE CASCADE,
+  contact_id int REFERENCES contacts(id) ON DELETE CASCADE,
+  primary key (group_id, contact_id)
 );
 
-create table addresses
+CREATE TABLE addresses
 (
-	account_id text,
-	address text,
-	city text,
-	country text,
-	home_address_contact_id bigint,
-	state text,
-	work_address_contact_id bigint,
-	zip text
+  account_id text,
+  address text,
+  city text,
+  country text,
+  home_address_contact_id int REFERENCES contacts(id) ON DELETE CASCADE,
+  state text,
+  work_address_contact_id int REFERENCES contacts(id) ON DELETE CASCADE,
+  zip text
 );
 
 ALTER TABLE contacts ADD COLUMN notes text;
-ALTER TABLE contacts ADD COLUMN profile_id int REFERENCES contacts(id);
+ALTER TABLE contacts ADD COLUMN profile_id int REFERENCES profiles(id) ON DELETE SET NULL;
